6e9b26f6744b5018eec23ffb14efc14d11c384d4
Seung Lee <sintaelz@gmail.com>
2017-01-10 09:50:25 -0600

    modif

--- Parcial1/Unitario/Recursive/first_unit.cc
+++ Parcial1/Unitario/Recursive/first_unit.cc
@@ -9,8 +9,8 @@ TEST(Factorial, positives)
 
 TEST(Factorial, negatives)
 {
-  ASSERT_EQ(-1, recursive(-1));
-  ASSERT_EQ(-120, recursive(-5));
+  ASSERT_EQ(1, recursive(-1));
+  ASSERT_EQ(120, recursive(-5));
 }
 
 TEST(Factorial, zero)
